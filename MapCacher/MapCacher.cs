2440843cc59e94408ecc10b04ed61fa8f9212bb3
Alec <me@kronks.me>
2015-04-01 10:06:09 -0700

    Begin work on WorldEditor and MapCacher

---
+++ MapCacher/MapCacher.cs
@@ -0,0 +1,33 @@
+﻿using Lib_K_Relay;
+using Lib_K_Relay.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapCacher
+{
+    public class MapCacher : IPlugin
+    {
+        public string GetAuthor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(Proxy proxy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WorldEdit/WorldEdit.cs
@@ -0,0 +1,27 @@
+﻿using Lib_K_Relay;
+using Lib_K_Relay.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldEdit
+{
+    public class WorldEdit : IPlugin
+    {
+        public string GetAuthor()
+        { return "KrazyShank / Kronks, thanks to 059"; }
+
+        public string GetName()
+        { return "Live World Editor"; }
+
+        public string GetDescription()
+        { return "Allows you to edit the tiles of the map while in game.\nUse the /worldedit command to begin."; }
+
+        public void Initialize(Proxy proxy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
