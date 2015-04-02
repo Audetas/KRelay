1cc32bdcdcd3bcd84f9eef14e1e3686c7c85f3f4
TheMrNobody <Anton.Sidorov@gmail.com>
2015-04-03 00:20:20 +1100

    [WIP] Reconnect plugin
    Probably 70% of work for release ready, ran into a problem with the
    notificationpacket...
    
    I got some good ideas for this later(after release) to make it more
    complex, for now I should be good with this

--- K_Relay/Util/PacketDebugger.cs
+++ K_Relay/Util/PacketDebugger.cs
@@ -2,6 +2,7 @@
 using Lib_K_Relay.Interface;
 using Lib_K_Relay.Networking;
 using Lib_K_Relay.Networking.Packets;
+using Lib_K_Relay.Networking.Packets.Client;
 using Lib_K_Relay.Networking.Packets.Server;
 using Lib_K_Relay.Util;
 using System;
@@ -15,7 +16,7 @@ namespace K_Relay.Util
     class PacketDebugger : IPlugin
     {
         private List<PacketType> _reportId = new List<PacketType>()
-        {
+        {PacketType.USEPORTAL, PacketType.RECONNECT
         };
 
         private List<PacketType> _printString = new List<PacketType>()
@@ -42,12 +43,40 @@ namespace K_Relay.Util
         {
             if (_reportId.Contains(packet.Type)) Console.WriteLine("[Packet Debugger] Received {0} packet.", packet.Type);
             if (_printString.Contains(packet.Type)) Console.WriteLine("[Packet Debugger] {0}", packet);
+
+            if (_reportId.Contains(packet.Type))
+            {
+                string text = "";
+                switch (packet.Type)
+                {
+                    /*case PacketType.HELLO:
+                        HelloPacket hPacket = packet as HelloPacket;
+                        text = string.Format("Packet Structure: BuildVersion : {0}, GameId: {1}, GUID: {2}, Id: {3}, Key: {4}, KeyTime: {5}, Obf1: {6}, Obf2 :{7}, Obf3: {8}, Obf4: {9}, Obf5: {10}, Obf6: {11}, Password: {12}, Random1: {13}, Random2: {14}, Secret: {15}.", hPacket.BuildVersion, hPacket.GameId, hPacket.GUID, hPacket.Id, hPacket.Key, hPacket.KeyTime, hPacket.Obf1, hPacket.Obf2, hPacket.Obf3, hPacket.Obf4, hPacket.Obf5, hPacket.Obf6, hPacket.Password, hPacket.Random1, hPacket.Random2, hPacket.Secret, hPacket.Send);
+                        break;
+                        */
+                    case PacketType.RECONNECT:
+                        ReconnectPacket rPacket = packet as ReconnectPacket;
+
+                        string bytestringified = "";
+                        foreach (byte eiii in rPacket.Key)
+                            bytestringified += ", " + eiii;
+
+                        text = string.Format("Packet Structure: GameId: {0}, Host: {1}, Id: {2}, IsFromArena: {3}, Key: {4}, KeyTime: {5}, Name: {6}, Port: {7}, Send: {8}", rPacket.GameId, rPacket.Host, rPacket.Id, rPacket.IsFromArena, bytestringified, rPacket.KeyTime, rPacket.Name, rPacket.Port, rPacket.Send);
+                        break;
+                    case PacketType.USEPORTAL:
+                        UsePortalPacket uPacket = packet as UsePortalPacket;
+                        text = string.Format("Packet Structure: Id: {0}, ObjectId: {1}, Send: {2}", uPacket.Id, uPacket.ObjectId, uPacket.Send);
+                        break;
+                }
+                Console.WriteLine("[Packet Debugger] " + text);
+            }
+
         }
 
         private void OnUpdatePacket(ClientInstance client, Packet packet)
         {
             UpdatePacket update = (UpdatePacket)packet;
-            
+
             for (int i = 0; i < update.Tiles.Length; i++)
             {
                 update.Tiles[i].Type = Serializer.Tiles["SpiderDirt"];
---
+++ Lib K Relay/Networking/Packets/Client/UsePortalPacket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_K_Relay.Networking.Packets.Client
+{
+    public class UsePortalPacket : Packet
+    {
+        public int ObjectId;
+
+        public override PacketType Type
+        { get { return PacketType.USEPORTAL; } }
+
+        public override void Read(PacketReader r)
+        {
+            this.ObjectId = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(ObjectId);
+        }
+    }
+}
