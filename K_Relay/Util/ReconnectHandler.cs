33b75bb9fe5245aa6ca5b79d674db8032b409ed5
Juix / Jex4334 <tuckerulrich@gmail.com>
2015-08-05 18:59:34 -0600

    Bug fixes, and added a vault command (Buggy)

--- K_Relay/Util/ReconnectHandler.cs
+++ K_Relay/Util/ReconnectHandler.cs
@@ -13,6 +13,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace K_Relay.Util
 {
@@ -35,25 +36,30 @@ namespace K_Relay.Util
                      "Use /connect ingame to change server.\n" +
                      "Use /recon to reconnect to your last realm.\n" +
                      "Use /drecon to reconnect to your last dungeon.\n" +
-                     "Use /serv to display your current server.\n";
+                     "Use /serv to display your current server.\n" +
+                     "Use /vault to go to your vault. (BUGGY!)\n";
         }
 
         public string[] GetCommands()
-        { return new string[] { "/connect", "/recon", "/drecon", "/serv" }; }
+        { return new string[] { "/connect", "/recon", "/drecon", "/serv", "/vault" }; }
 
         public void Initialize(Proxy proxy)
         {
             _proxy = proxy;
             proxy.HookPacket(PacketType.RECONNECT, OnReconnect);
             proxy.HookPacket(PacketType.CREATESUCCESS, OnCreateSuccess);
+            proxy.HookPacket(PacketType.NEWTICK, OnNewTick);
             proxy.HookCommand("serv", OnServCommand);
             proxy.HookCommand("connect", OnConnectCommand);
             proxy.HookCommand("recon", OnReconnectCommand);
             proxy.HookCommand("drecon", OnDReconnectCommand);
+            proxy.HookCommand("vault", OnVaultCommand);
+            //proxy.HookCommand("yard", OnYardCommand);
+            //proxy.HookCommand("hall", OnHallCommand);
         }
 
         private ConClient getClient(Client client)
-        {
+        {            
             if (clients.ContainsKey(client.PlayerData.Name))
             {
                 return clients[client.PlayerData.Name];
@@ -65,28 +71,130 @@ namespace K_Relay.Util
             }
         }
 
+        private ConClient getClientFromName(string name)
+        {
+            if (clients.ContainsKey(name))
+            {
+                return clients[name];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void OnNewTick(Client client, Packet packet)
+        {
+            ConClient _c = getClient(client);
+            _c._toConnect = client;
+
+            if (!_c.hasWelcomed)
+            {
+                if (client.PlayerData != null)
+                {
+                    if (client.PlayerData.Name != null && client.PlayerData.Name != "")
+                    {
+                        // Send welcome message to player
+                        string message = "Welcome to K Relay!";
+                        message += "\\n";
+                        if (_c.currentServ == null)
+                        {
+                            foreach (var pair in Serializer.Servers)
+                            {
+                                if (pair.Value == _proxy.getRemoteAddress(client))
+                                {
+                                    message += pair.Key;
+                                    _c.setCurrentServer(pair.Key);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            message += _c.currentServ;
+                        }
+                        message += " " + _c.lastRealm;
+
+                        PluginUtils.Delay(1500, () => client.SendToClient(
+                            PluginUtils.CreateNotification(client.ObjectId, message)));
+                        _c.hasWelcomed = true;
+                    }
+                }
+            }
+            
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Plugins\\Reconnect\\");
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\Plugins\\Reconnect\\Change.text"))
+            {
+                string[] lines = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\Plugins\\Reconnect\\Change.text");
+                if (lines.Length > 0)
+                {
+                    if (lines[0] != null && lines[0] != "")
+                    {
+                        string[] data = lines[0].Split(',');
+                        if (getClientFromName(data[1]) != null)
+                        {
+                            ConClient cl = getClientFromName(data[1]);
+                            cl.reconnectData = data;
+                            cl.reconnectReady = true;
+                        }
+                    }
+                }
+                File.Delete(Directory.GetCurrentDirectory() + "\\Plugins\\Reconnect\\Change.text");
+            }
+
+            if (_c.reconnectReady)
+            {
+                if (_c.reconnectData[0] == "server")
+                {
+                    ChangeServer(Serializer.Servers[_c.reconnectData[2]], _c.reconnectData[2], _c._toConnect);
+                }
+                else if (_c.reconnectData[0] == "realm")
+                {
+                    //ReconnectPacket _recon = (ReconnectPacket)Packet.Create(PacketType.RECONNECT);
+                    _c._recon.IsFromArena = false;
+                    _c._recon.GameId = 0;
+                    _c._recon.Host = _c.reconnectData[3];
+                    _c._recon.Port = 2050;
+                    _c._recon.Key = new byte[0];
+                    _c._recon.KeyTime = 0;//int.Parse(_c.reconnectData[4]);
+                    _c._recon.Name = "NexusPortal." + _c.reconnectData[2];
+
+                    Console.WriteLine(_c._recon.ToString());
+
+                    SendReconnect(_c._recon, _c._toConnect);
+                }
+                _c.reconnectReady = false;
+            }
+            
+        }
+
         private void OnCreateSuccess(Client client, Packet packet)
         {
             ConClient _c = getClient(client);
 
+            /*
             // Send welcome message to player
             string message = "Welcome to K Relay!";
+            message += "\\n";
             foreach (var pair in Serializer.Servers)
             {
                 if (pair.Value == _proxy.getRemoteAddress(client))
                 {
-                    message += "\\n" + pair.Key;
+                    message += pair.Key;
                     _c.setCurrentServer(pair.Key);
                 }
             }
 
+            message += " " + _c.lastRealm;
+
             PluginUtils.Delay(1500, () => client.SendToClient(
                 PluginUtils.CreateNotification(client.ObjectId, message)));
+            */
         }
 
         private void OnReconnect(Client client, Packet packet)
         {
             ConClient _c = getClient(client);
+            _c.hasWelcomed = false;
 
             ReconnectPacket reconnect = packet as ReconnectPacket;
 
@@ -103,6 +211,8 @@ namespace K_Relay.Util
                 _c._recon.KeyTime = reconnect.KeyTime;
                 _c._recon.Name = reconnect.Name;
                 string[] names = reconnect.Name.Split('.');
+                _c.lastRealmIP = reconnect.Host;
+                _c.lastRealmTime = reconnect.KeyTime.ToString();
                 _c.setRealm(names[1]);
             }
             else if (reconnect.Name != "" && !reconnect.Name.Contains("vault") && reconnect.GameId != -2)
@@ -123,14 +233,41 @@ namespace K_Relay.Util
             else if (reconnect.Name.ToLower().Contains("vault"))
             {
                 _c.setRealm("Vault");
+                /*
+                _c._vault.IsFromArena = false;
+                _c._vault.GameId = reconnect.GameId;
+                _c._vault.Host = (reconnect.Host == "" ? _proxy.getRemoteAddress(client) : reconnect.Host);
+                _c._vault.Port = (reconnect.Port == -1 ? _proxy.Port : reconnect.Port);
+                _c._vault.Key = reconnect.Key;
+                _c._vault.KeyTime = reconnect.KeyTime;
+                _c._vault.Name = reconnect.Name;
+                */
             }
             else if (reconnect.Name.ToLower().Contains("pet"))
             {
                 _c.setRealm("Pet Yard");
+                
+                _c._yard.IsFromArena = false;
+                _c._yard.GameId = reconnect.GameId;
+                _c._yard.Host = (reconnect.Host == "" ? _proxy.getRemoteAddress(client) : reconnect.Host);
+                _c._yard.Port = (reconnect.Port == -1 ? _proxy.Port : reconnect.Port);
+                _c._yard.Key = reconnect.Key;
+                _c._yard.KeyTime = reconnect.KeyTime;
+                _c._yard.Name = reconnect.Name;
+                
             }
             else if (reconnect.Name.ToLower().Contains("guild"))
             {
                 _c.setRealm("Guild Hall");
+                
+                _c._ghall.IsFromArena = false;
+                _c._ghall.GameId = reconnect.GameId;
+                _c._ghall.Host = (reconnect.Host == "" ? _proxy.getRemoteAddress(client) : reconnect.Host);
+                _c._ghall.Port = (reconnect.Port == -1 ? _proxy.Port : reconnect.Port);
+                _c._ghall.Key = reconnect.Key;
+                _c._ghall.KeyTime = reconnect.KeyTime;
+                _c._ghall.Name = reconnect.Name;
+                
             }
 
             if (reconnect.Port != -1)
@@ -145,6 +282,8 @@ namespace K_Relay.Util
                 _proxy.defTempServer = _proxy.getRemoteAddress(client);
             }
 
+            //Console.WriteLine(reconnect.ToString());
+
             // Tell the client to connect to the proxy
             reconnect.Host = "localhost";
             reconnect.Port = 2050;
@@ -155,6 +294,27 @@ namespace K_Relay.Util
             ConClient _c = getClient(client);
 
             client.SendToClient(PluginUtils.CreateNotification(client.ObjectId, _c.currentServ + " " + _c.lastRealm));
+        }
+
+        private void OnVaultCommand(Client client, string command, string[] args)
+        {
+            ConClient _c = getClient(client);
+
+            SendReconnect(_c._vault, client);
+        }
+
+        private void OnYardCommand(Client client, string command, string[] args)
+        {
+            ConClient _c = getClient(client);
+
+            SendReconnect(_c._yard, client);
+        }
+
+        private void OnHallCommand(Client client, string command, string[] args)
+        {
+            ConClient _c = getClient(client);
+
+            SendReconnect(_c._ghall, client);
         }
 
         private void OnConnectCommand(Client client, string command, string[] args)
@@ -200,6 +360,9 @@ namespace K_Relay.Util
 
         private void SendReconnect(ReconnectPacket reconnect, Client client)
         {
+            ConClient _c = getClient(client);
+            _c.hasWelcomed = false;
+
             string host = reconnect.Host;
             int port = reconnect.Port;
             if (!host.ToLower().Contains("localhost")) _proxy.defTempServer = host;
