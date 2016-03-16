803a419024c21adde3b370fe4494fdce12624a3a
TheKronks <qwertyuiop580@gmail.com>
2016-03-16 16:36:14 -0700

    New official icon thanks to 059

---
+++ Lib K Relay/Networking/Client.PacketLogic.cs
@@ -0,0 +1,18 @@
+﻿using Lib_K_Relay.Networking.Packets;
+using Lib_K_Relay.Networking.Packets.Client;
+using Lib_K_Relay.Networking.Packets.DataObjects;
+using Lib_K_Relay.Networking.Packets.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_K_Relay.Networking
+{
+    public partial class Client
+    {
+        private MapInfoPacket _mapInfo;
+    }
+}
