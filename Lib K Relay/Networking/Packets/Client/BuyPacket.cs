71dfa13b43bf471c1f70ae6d86a7927e766a87aa
kecleon <ultimatedood@gmail.com>
2016-03-16 18:58:55 -0700

    BUY packet fix
    quantity is now a variable as of a recent rotmg build

--- Lib K Relay/Networking/Packets/Client/BuyPacket.cs
+++ Lib K Relay/Networking/Packets/Client/BuyPacket.cs
@@ -9,6 +9,7 @@ namespace Lib_K_Relay.Networking.Packets.Client
     public class BuyPacket : Packet
     {
         public int ObjectId;
+		public int Quantity;
 
         public override PacketType Type
         { get { return PacketType.BUY; } }
@@ -16,11 +17,13 @@ namespace Lib_K_Relay.Networking.Packets.Client
         public override void Read(PacketReader r)
         {
             ObjectId = r.ReadInt32();
+			Quantity = r.ReadInt32();
         }
 
         public override void Write(PacketWriter w)
         {
             w.Write(ObjectId);
+			w.Write(Quantity);
         }
     }
 }
