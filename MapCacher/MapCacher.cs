9c4cb896bd6e87ec7ee4f3f357dc14762a3423a5
apemanzilla <dominic@marcuse.us>
2016-07-26 22:40:34 -0400

    I think I fixed the bug finally

--- MapCacher/MapCacher.cs
+++ MapCacher/MapCacher.cs
@@ -11,35 +11,46 @@ using Lib_K_Relay.Networking.Packets.DataObjects;
 using Lib_K_Relay.Networking.Packets.Server;
 using Lib_K_Relay.Networking.Packets.Client;
 using Lib_K_Relay.Utilities;
+using System.Text;
 
 namespace MapCacher {
-	class Map {
-		public uint fp;
-		public int Width, Height;
-		public ushort[,] Data;
-		public string Name;
-
-		public Map(MapInfoPacket mapInfo) {
-			fp = mapInfo.Fp;
-			Width = mapInfo.Width;
-			Height = mapInfo.Height;
-			Data = new ushort[Width, Height];
-			Name = mapInfo.Name;
+	static class Extensions {
+		public static T Shift<T>(this List<T> list) {
+			if (list.Count < 1) throw new IndexOutOfRangeException("List is empty.");
+			T output = list[0];
+			list.RemoveAt(0);
+			return output;
 		}
 
-		public void Clear() {
+		public static List<T> Shift<T>(this List<T> list, int count) {
+			count = Math.Min(count, list.Count);
+			List<T> output = list.GetRange(0, count);
+			list.RemoveRange(0, count);
+			return output;
+		}
+	}
+
+	class Map {
+		public uint UUID;
+		public int Width, Height;
+		public ushort[,] Data;
+
+		public Map(MapInfoPacket info) {
+			UUID = info.Fp;
+			Width = info.Width;
+			Height = info.Height;
 			Data = new ushort[Width, Height];
 		}
 	}
 
 	public class MapCacher : IPlugin {
-		public static readonly int MaxTilesSent = 64;
-
-		Dictionary<Client, List<Tile>> SendQueue = new Dictionary<Client, List<Tile>>();
+		public static readonly int MaxTilesPerPacket = 256;
 
 		Dictionary<uint, Map> CachedMaps = new Dictionary<uint, Map>();
 
 		Dictionary<Client, Map> CurrentMaps = new Dictionary<Client, Map>();
+
+		Dictionary<Client, List<Tile>> SendQueues = new Dictionary<Client, List<Tile>>();
 
 		public string GetAuthor() {
 			return "apemanzilla";
@@ -60,90 +71,52 @@ namespace MapCacher {
 		}
 
 		public void Initialize(Proxy proxy) {
-			proxy.ClientConnected += OnConnect;
-			proxy.ClientDisconnected += OnDisconnect;
-
 			proxy.HookPacket(PacketType.MAPINFO, OnMapInfo);
 			proxy.HookPacket(PacketType.UPDATE, OnUpdate);
-
-			proxy.HookCommand("mapcacher", OnCommand);
-		}
-
-		void OnCommand(Client client, string command, string[] args) {
-			if (args.Length < 1) {
-				client.SendToClient(PluginUtils.CreateOryxNotification("Map Cacher", "Usage:"));
-				client.SendToClient(PluginUtils.CreateOryxNotification("Map Cacher", "'/mapcacher clear' - clears all currently cached maps."));
-				return;
-			}
-			switch (args[0]) {
-				case "clear":
-					CachedMaps.ForEach(cached => cached.Value.Clear());
-					client.SendToClient(PluginUtils.CreateOryxNotification("Map Cacher", "All maps were cleared."));
-					break;
-			}
-		}
-
-		void OnConnect(Client client) {
-			if (SendQueue.ContainsKey(client)) SendQueue.Remove(client);
-
-			SendQueue[client] = null;
-		}
-
-		void OnDisconnect(Client client) {
-			if (SendQueue.ContainsKey(client)) SendQueue.Remove(client);
-			if (CurrentMaps.ContainsKey(client)) CurrentMaps.Remove(client);
 		}
 
 		void OnMapInfo(Client client, Packet p) {
 			MapInfoPacket mapInfo = p as MapInfoPacket;
 
-			if (CachedMaps.Any(m => m.Value.fp == mapInfo.Fp)) {
-				// map exists
-				Map map = CachedMaps.First(m => m.Value.fp == mapInfo.Fp).Value;
-
+			Map map;
+			if (CachedMaps.ContainsKey(mapInfo.Fp)) {
+				map = CachedMaps[mapInfo.Fp];
 				CurrentMaps[client] = map;
 
-				PluginUtils.Log("Map Cacher", "Loaded cached map {0} ({1}x{2}, uuid {3})", map.Name, map.Width, map.Height, map.fp);
+				// queue cached tiles
+				SendQueues[client] = new List<Tile>();
+				for (short x = 0; x < map.Width; x++)
+					for (short y = 0; y < map.Height; y++)
+						if (map.Data[x, y] != 0)
+							SendQueues[client].Add(new Tile { X = x, Y = y, Type = map.Data[x, y] });
+#if DEBUG
+				PluginUtils.Log("Map Cacher", "Loaded cached map {0}", mapInfo.Name);
+#endif
 			} else {
-				// map not cached
-				Map map = new Map(mapInfo);
-
+				map = new Map(mapInfo);
+				CachedMaps[map.UUID] = map;
 				CurrentMaps[client] = map;
-				CachedMaps[mapInfo.Fp] = map;
-				SendQueue[client] = new List<Tile>();
+				SendQueues[client] = new List<Tile>(); // no tiles cached yet
 
-				PluginUtils.Log("Map Cacher", "Loaded new map {0} ({1}x{2}, uuid {3})", map.Name, map.Width, map.Height, map.fp);
+#if DEBUG
+				PluginUtils.Log("Map Cacher", "Loaded new map {0}", mapInfo.Name);
+#endif
 			}
 		}
 
 		void OnUpdate(Client client, Packet p) {
 			UpdatePacket update = p as UpdatePacket;
 
-			if (CurrentMaps.ContainsKey(client)) {
-				Map current = CurrentMaps[client];
+			foreach (Tile tile in update.Tiles)
+				CurrentMaps[client].Data[tile.X, tile.Y] = tile.Type;
 
-				foreach (Tile t in update.Tiles) {
-					current.Data[t.X, t.Y] = t.Type;
+			if (SendQueues[client].Count > 0) {
+				List<Tile> newTiles = new List<Tile>(update.Tiles);
+				SendQueues[client].Shift(Math.Min(MaxTilesPerPacket, Math.Max(0, MaxTilesPerPacket - newTiles.Count))).ForEach(tile => newTiles.Add(tile));
+				update.Tiles = newTiles.ToArray();
 
-					if (SendQueue[client] != null) {
-						SendQueue[client].Add(t);
-					}
-				}
-
-				if (SendQueue[client] == null) {
-					List<Tile> tiles = new List<Tile>(update.Tiles);
-
-					for (short x = 0; x < current.Width; x++)
-						for (short y = 0; y < current.Height; y++)
-							if (current.Data[x, y] != 0)
-								tiles.Add(new Tile() { X = x, Y = y, Type = current.Data[x, y] });
-
-					SendQueue[client] = tiles;
-				}
-
-				if (SendQueue[client].Count > 0) {
-					update.Tiles = SendQueue[client].Take(MaxTilesSent).ToArray();
-					SendQueue[client].RemoveAll(t => update.Tiles.Contains(t));
+				if (SendQueues[client].Count == 0) {
+					PluginUtils.Log("Map Cacher", "Sent cached data to {0}", client.PlayerData.Name ?? "[unknown player]");
 				}
 			}
 		}
