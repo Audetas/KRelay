65b016dafe550e85815de7889ae73a6c6fc343ea
Alec Audet <qwertyuiop580@gmail.com>
2016-10-12 21:44:15 -0700

    Update SlotObject for 27.7.X6

---
+++ Lib%20K%20Relay/Networking/Packets/DataObjects/SlotObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_K_Relay.Networking.Packets.DataObjects
+{
+    public class SlotObject : IDataObject
+    {
+        public int ObjectId;
+        public byte SlotId;
+        public int ObjectType;
+
+        public IDataObject Read(PacketReader r)
+        {
+            ObjectId = r.ReadInt32();
+            SlotId = r.ReadByte();
+            ObjectType = r.ReadInt32();
+
+            return this;
+        }
+
+        public void Write(PacketWriter w)
+        {
+            w.Write(ObjectId);
+            w.Write(SlotId);
+            w.Write(ObjectType);
+        }
+
+        public object Clone()
+        {
+            return new SlotObject
+            {
+                ObjectId = this.ObjectId,
+                ObjectType = this.ObjectType,
+                SlotId = this.SlotId
+            };
+        }
+
+        public override string ToString()
+        {
+            return "{ ObjectId=" + ObjectId + ", SlotId=" + SlotId + ", ObjectType=" + ObjectType + " }";
+        }
+    }
+}
