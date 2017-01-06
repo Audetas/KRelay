95987eef3aa662fb9a0f24877cd584c67a7d0fdf
Alec Audet <me@kronks.me>
2017-01-05 19:22:58 -0800

    Converted FameNotifier, Glow, ClientStatAnnouncer

---
+++ Plugins/FameNotifier/FameNotifier.cs
@@ -0,0 +1,46 @@
+﻿using LibKRelay;
+using LibKRelay.Messages;
+using LibKRelay.Messages.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FameNotifier
+{
+    public class FameNotifier : Plugin
+    {
+        private Dictionary<Connection, int> _fame = new Dictionary<Connection, int>();
+
+        public string GetAuthor()
+        { return "KrazyShank / Kronks"; }
+
+        public string GetName()
+        { return "Fame Notifer"; }
+
+        public string GetDescription()
+        { return "Lets you know when your fame has increased."; }
+
+        public string[] GetCommands()
+        { return new string[] { }; }
+
+        public override void Initialize(ClientListener proxy)
+        {
+            Message.Hook<NewTick>(OnNewTick);
+            proxy.ClientConnected += (client) => _fame.Add(client, -1);
+            proxy.ClientDisconnected += (client) => _fame.Remove(client);
+        }
+
+        private void OnNewTick(Connection con, NewTick newTick)
+        {
+            int fame = _fame[con];
+            _fame[con] = con.Self[StatType.CharacterFame];
+
+            if (fame != -1 && con.Self[StatType.CharacterFame] != fame)
+                con.Client.Send(
+                    CreateNotification(
+                        con.Self.ObjectId, "+" + (con.Self[StatType.CharacterFame] - fame) + " fame!"));
+        }
+    }
+}
---
+++ Plugins/Glow/Glow.cs
@@ -0,0 +1,57 @@
+﻿using LibKRelay;
+using LibKRelay.Messages;
+using LibKRelay.Messages.Client;
+using LibKRelay.Messages.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glow
+{
+    public class Glow : Plugin
+    {
+        public string GetAuthor()
+        { return "KrazyShank / Kronks"; }
+
+        public string GetName()
+        { return "Glow"; }
+
+        public string GetDescription()
+        { return "You're so excited about K Relay that you're literally glowing!"; }
+
+        public string[] GetCommands()
+        { return new string[] { "/AmISpecial" }; }
+
+        public override void Initialize(ClientListener listener)
+        {
+            Message.Hook<Update>(OnUpdate);
+            Message.Hook<PlayerText>(OnPlayerText);
+        }
+
+        private void OnPlayerText(Connection con, PlayerText message)
+        {
+            if (message.IsCommand("amispecial"))
+            {
+                Random r = new Random();
+                int val = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    val += r.Next(400000, 723411);
+                    con.Client.Send(CreateNotification(
+                        con.Self.ObjectId, val, "YOU ARE SPECIAL!"));
+                }
+            }
+        }
+
+        private void OnUpdate(Connection client, Update update)
+        {
+            Entity player;
+            if (update.NewObjs.TryGetValue(client.Self.ObjectId, out player))
+            {
+                player[StatType.Glowing].IntValue = 100;
+            }
+        }
+    }
+}
